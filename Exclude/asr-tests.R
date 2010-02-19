77005f86f597ae095b2de93a02d2a8f236bb1017
Emma Goldberg <emma@skink.(none)>
2010-02-19 16:26:00 -0700

    Binary reconstruction test results.
    Added .anc files with results for a variety of root assumptions with
    diversitreeNF's reconstruct.bisse() and just the condlike root (only one
    available) with diversitree's asr.marginal().
    
    Agreement is perfect between the packages when root is condlike and
    condition.surv=F.

--- Exclude/asr-tests.R
+++ Exclude/asr-tests.R
@@ -3,14 +3,21 @@
 ### v0.4-4, Feb 19, 2010
 
 source("/home/emma/src/miscR/ttn.R")
+mycol <- c(rgb(0.832, 0.367, 0), rgb(0, 0.445, 0.695))  # orange, blue
 
 ttn <- read.ttn("example-clean.ttn", nodes=F)
+plot(ttn$tree, show.tip.label=F, no.margin=T)
+tiplabels(text=as.character(seq(ttn$tree$Nnode+1)), bg=mycol[ttn$states+1],
+          adj=0)
+nodelabels()
+
 params <- c(1.4, 0.4, 0.2, 0.1, 0.6, 0.35)
 names(params) <- c("lambda0", "lambda1", "mu0", "mu1", "q01", "q10")
 
-# ROOT.OBS  = 3 (conditional likelihoods, the default)
-# ROOT.EQUI = 2 (stationary)
-# ROOT.FLAT = 1 (equal weightings)
+# ROOT.OBS   = 3 (conditional likelihoods, the default)
+# ROOT.EQUI  = 2 (stationary)
+# ROOT.FLAT  = 1 (equal weights)
+# ROOT.GIVEN = 4 (give weights with root.p)
 
 #--------------------------------------------------
 # Part I: node.fixing (eeg)
@@ -22,7 +29,7 @@ names(params) <- c("lambda0", "lambda1", "mu0", "mu1", "q01", "q10")
 library(diversitreeNF)
 
 # condlike root
-ans <- reconstruct.bisse(ttn$tree, ttn$states, params)
+ans <- reconstruct.bisse(ttn$tree, ttn$states, params, condition.surv=F)
 ans <- data.frame(label=ttn$tree$node.label, ans)
 i <- order(ans$label)
 printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
@@ -30,7 +37,7 @@ write.table(printme, file="condlike-eeg.anc", quote=FALSE, sep="\t",
             row.names=FALSE, col.names=FALSE)
 
 # stationary root
-ans <- reconstruct.bisse(ttn$tree, ttn$states, params, root=2)
+ans <- reconstruct.bisse(ttn$tree, ttn$states, params, condition.surv=F, root=2)
 ans <- data.frame(label=ttn$tree$node.label, ans)
 i <- order(ans$label)
 printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
@@ -38,18 +45,48 @@ write.table(printme, file="stationary-eeg.anc", quote=FALSE, sep="\t",
             row.names=FALSE, col.names=FALSE)
 
 # uniform root
-ans <- reconstruct.bisse(ttn$tree, ttn$states, params, root=1)
+ans <- reconstruct.bisse(ttn$tree, ttn$states, params, condition.surv=F, root=1)
 ans <- data.frame(label=ttn$tree$node.label, ans)
 i <- order(ans$label)
 printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
-write.table(printme, file="uniform-nf.eeg", quote=FALSE, sep="\t",
+write.table(printme, file="uniform-eeg.anc", quote=FALSE, sep="\t",
             row.names=FALSE, col.names=FALSE)
 
+# root fixed to state 0
+ans <- reconstruct.bisse(ttn$tree, ttn$states, params, condition.surv=F,
+                         root=4, root.p=c(1,0))
+ans <- data.frame(label=ttn$tree$node.label, ans)
+i <- order(ans$label)
+printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
+write.table(printme, file="fix0-eeg.anc", quote=FALSE, sep="\t",
+            row.names=FALSE, col.names=FALSE)
 
 #--------------------------------------------------
 # Part II: Rich (rgf)
 #-------------------------------------------------- 
 
 library(diversitree)
+?asr.marginal
 
-# only allows condlike root
+# Looks like only condlike root is allowed for asr.marginal().
+# The root.state argument only gets passed to asr.joint().
+
+lnL <- make.bisse(ttn$tree, ttn$states)
+
+# condlike root
+ans <- asr.marginal(lnL, params)
+ans <- data.frame(label=ttn$tree$node.label, p1=ans[2,])
+i <- order(ans$label)
+printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
+write.table(printme, file="condlike-rgf.anc", quote=FALSE, sep="\t",
+            row.names=FALSE, col.names=FALSE)
+# agrees perfectly with my eeg
+
+# stationary root
+ans <- asr.marginal(lnL, params, root.state=2)
+ans <- data.frame(label=ttn$tree$node.label, p1=ans[2,])
+i <- order(ans$label)
+printme <- data.frame(ans$label[i], round(ans$p1[i], 6))
+write.table(printme, file="stationary-rgf.anc", quote=FALSE, sep="\t",
+            row.names=FALSE, col.names=FALSE)
+# root.state appears to have no effect -- results identical to condlike
